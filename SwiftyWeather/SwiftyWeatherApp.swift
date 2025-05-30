1df3e288e58e9f8f71e4b4bf066bd65ad2707de8
Robert Beachill <bbeachill@icloud.com>
2025-05-30 23:09:42 +0100

    Initial Commit

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Robert Beachill on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
